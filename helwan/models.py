0d452837468e91057a9513522fbe925da7d8ef84
Amr3zzat <3mr3zzat@gmail.com>
2018-01-20 01:49:57 +0200

    Models , Urls

--- helwan/models.py
+++ helwan/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    grade = models.IntegerField
+    department = models.CharField(max_length=100)
--- helwan/views.py
+++ helwan/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import password_reset, password_reset_confirm
+
 
 # Create your views here.
+
+
+def home(request):
+    return render(request,'index.html')
+
+
+def signin(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    if 'next' in request.GET:
+                        return HttpResponseRedirect(request.GET['next'])
+                    else:
+                        return HttpResponseRedirect('/')
+                else:
+                    messages.add_message(request, messages.ERROR, 'Your account is desactivated.')
+                    return render(request, 'auth/signin.html')
+            else:
+                messages.add_message(request, messages.ERROR, 'Username or password invalid.')
+                return render(request, 'auth/signin.html')
+        else:
+            return render(request, 'auth/signin.html')
