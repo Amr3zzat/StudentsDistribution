66da5f438f72f8bf18d8939a68fb19daf1e18eb3
Amr3zzat <3mr3zzat@gmail.com>
2018-01-20 14:58:46 +0200

    Models , Urls

--- helwan/admin.py
+++ helwan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import student
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+
+
+admin.site.register(student)
--- helwan/urls.py
+++ helwan/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url ,include
 from . import views
 urlpatterns = [
     url(r'^$',views.home),
-    url(r'^login/',views.login)
 
 ]
