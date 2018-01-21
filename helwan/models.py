c2af5be318c16858322e528eff5554d1e55d74a6
Amr3zzat <3mr3zzat@gmail.com>
2018-01-21 23:12:55 +0200

    Models , Urls Roles , department added to users models all added to the admin

--- helwan/models.py
+++ helwan/models.py
@@ -1,8 +1,35 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class student(models.Model):
+class Profile(models.Model):
+    STUDENT = 1
+    Professor = 2
+    Head = 3
+    ROLE_CHOICES = (
+        (STUDENT, 'STUDENT'),
+        (Professor, 'Professor'),
+        (Head, 'Head'),
+    )
+    Computers = 1
+    Communications = 2
+    Dep_CHOICES = (
+        (Computers, 'Computers'),
+        (Communications, 'Communications'),
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    grade = models.IntegerField
-    department = models.CharField(max_length=100)
+    dep = models.PositiveSmallIntegerField(choices=Dep_CHOICES, null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
